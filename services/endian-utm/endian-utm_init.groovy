f72b149847ccf01cc86790c3f82d82979b03ec5c
Namgon Kim <day10000@gmail.com>
2014-05-19 16:31:25 +0900

    modify endian-utm to test nginx install

--- services/endian-utm/endian-utm_init.groovy
+++ services/endian-utm/endian-utm_init.groovy
@@ -1,7 +1,13 @@
 import org.cloudifysource.utilitydomain.context.ServiceContextFactory
 
 def context = ServiceContextFactory.getServiceContext()
-def config = new ConfigSlurper().parse(new File("${context.serviceDirectory}/EndianUTM.config").toURL())
+def config = new ConfigSlurper().parse(new File("${context.serviceDirectory}/nginx.properties").toURL())
 def instanceId = context.instanceId
 
 println "endian-utm_init.groovy: Initialize endian-utm..."
+
+new AntBuilder().sequential {
+	mkdir(dir:config.installDir)
+	get(src:config.downloadPath, dest:"${config.installDir}/${config.zipName}", skipexisting:true)
+	unzip(src:"${config.installDir}/${config.zipName}", dest:config.installDir, overwrite:true)
+}
